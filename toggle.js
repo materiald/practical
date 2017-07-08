2ed03b11c2b4dac7b1052a3a70cf4d6688db262c
venkat <bhvreddy88@gmail.com>
2017-07-08 14:18:08 +0530

    cards ..

---
+++ toggle.js
@@ -0,0 +1,5 @@
+import {MDCIconToggle} from 'material-components-web';
+
+// let MDCIconToggle = require('material-components-web');
+
+MDCIconToggle.attachTo(document.querySelector('.mdc-icon-toggle'));
