0c00339128ce1392814666b36fccb9ab02725707
venkat <bhvreddy88@gmail.com>
2017-07-10 12:28:46 +0530

    ..

--- a.js
+++
@@ -1,6 +0,0 @@
-import { x, y } from './b.js';
-
-
-a = () => {
-    console.log('from a - a.js ');
-}
--- b.js
+++
@@ -1,7 +0,0 @@
-let x = console.log('this is x form b.js');
-
-let y = () => {
-    console.log('this is y from b.js');
-}
-
-export { x, y };
