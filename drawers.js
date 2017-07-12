ef5e61e884be04ebfe22a7a31c4d33a837139162
venkat <bhvreddy88@gmail.com>
2017-07-12 12:58:18 +0530

    drawers .. temporary-drawer working fine

---
+++ drawers.js
@@ -0,0 +1,23 @@
+var drawerEl = document.querySelector('.mdc-temporary-drawer');
+var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
+var drawer = new MDCTemporaryDrawer(drawerEl);
+document.querySelector('.menu').addEventListener('click', function () {
+    drawer.open = !drawer.open;
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:open', function () {
+    console.log('Received MDCTemporaryDrawer:open');
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:close', function () {
+    console.log('Received MDCTemporaryDrawer:close');
+});
+
+
+
+
+let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
+document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
+
+
+
+// var drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector("#page__drawer"));
+// document.querySelector("#drawer-nav-button").addEventListener("click", function() { drawer.open = true; });
