09beba59887a8a66c8a61f39a1bcb3d995e113cc
venkat <bhvreddy88@gmail.com>
2017-07-14 09:37:23 +0530

    dialog - working fine

--- dialogs.js
+++ dialogs.js
@@ -1,3 +1,23 @@
-import {MDCDialog, MDCDialogFoundation, util} from '@material/dialog';
+// import {MDCDialog, MDCDialogFoundation, util} from '@material/dialog';
+// mdc.dialog.MDCDialog.attachTo(document.querySelector('#my-mdc-dialog'));
 
-mdc.dialog.MDCDialog.attachTo(document.querySelector('#my-mdc-dialog'));
+
+
+// manual installation 
+// const dialog2 = new MDCDialog(document.querySelector('#my-mdc-dialog'));
+
+
+var dialog = new mdc.dialog.MDCDialog(document.querySelector('#my-mdc-dialog'));
+
+dialog.listen('MDCDialog:accept', function() {
+  console.log('accepted');
+})
+
+dialog.listen('MDCDialog:cancel', function() {
+  console.log('canceled');
+})
+
+document.querySelector('.default-dialog-activation').addEventListener('click', function (evt) {
+  dialog.lastFocusedTarget = evt.target;
+  dialog.show();
+})
